d53bc3b87f7287d514d936ca64ec0a7d917287fe
grigoriirarog <rarog.grigoriii@gmail.com>
2023-12-01 21:19:46 +0100

    Написал java doc. Добави зависимость для юнит тестов

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -1,77 +1,31 @@
 package com.gridnine.testing;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        FlightFilterService service = new FlightFilterServiceImpl();
         List<Flight> flights = FlightBuilder.createFlights();
+
         System.out.println();
+        System.out.println("All flights:");
+        printer(flights);
+        System.out.println();
+        System.out.println("Removed flights departing in the past:");
+        List<Flight> filter1 = service.flightDepartingInThePastRemover(flights);
+        printer(filter1);
+        System.out.println();
+        System.out.println("Removed flights that departs before it arrives:");
+        List<Flight> filter2 = service.flightThatDepartsBeforeItArrivesRemover(flights);
+        printer(filter2);
+        System.out.println();
+        System.out.println("Removed flights with more than two hours ground time:");
+        List<Flight> filter3 = service.flightWithMoreThanTwoHoursGroundTimeRemover(flights);
+        printer(filter3);
+    }
+    private static void printer(List<Flight> flights) {
         for (Flight f : flights) {
             System.out.println(f);
-        }
-        System.out.println("------------------------------------------------------------------------------------------------------");
-        filter1();
-        System.out.println("------------------------------------------------------------------------------------------------------");
-        filter2();
-        System.out.println("------------------------------------------------------------------------------------------------------");
-        filter3();
-        System.out.println("------------------------------------------------------------------------------------------------------");
-    }
-
-    public static void filter1() {
-        List<Flight> flights = FlightBuilder.createFlights();
-
-        flights.forEach(flight -> {
-            if (flight.getSegments().stream()
-                    .anyMatch(segment -> !segment.getDepartureDate().isBefore(LocalDateTime.now()))) {
-                System.out.println(flight);
-            }
-        });
-    }
-
-    public static void filter2() {
-        List<Flight> flights = FlightBuilder.createFlights();
-
-        flights.forEach(flight -> {
-            if (flight.getSegments().stream()
-                    .anyMatch(segment -> !segment.getDepartureDate().isAfter(segment.getArrivalDate()))) {
-                System.out.println(flight);
-            }
-        });
-    }
-
-    public static void filter3() {
-        List<Flight> flights = FlightBuilder.createFlights();
-
-        for (Flight flight : flights) {
-            List<Segment> segments = flight.getSegments();
-            if (segments.size() > 1) {
-                LocalDateTime arrival = null;
-                LocalDateTime departure;
-                int waitingPeriod = 0;
-
-                for (int i = 0; i < segments.size(); i++) {
-                    if (i == 0) {
-                        Segment rightPart = segments.get(i);
-                        arrival = rightPart.getArrivalDate();
-                    }
-                    else {
-                        Segment leftPart = segments.get(i);
-                        departure = leftPart.getDepartureDate();
-                        Duration duration = Duration.between(arrival, departure);
-                        waitingPeriod += duration.toHoursPart();
-                        Segment rightPart = segments.get(i);
-                        arrival = rightPart.getArrivalDate();
-                    }
-                }
-                if (waitingPeriod < 3) {
-                    System.out.println(flight);
-                }
-            } else {
-                System.out.println(flight);
-            }
         }
     }
 }
---
+++ src/test/java/com/gridnine/testing/FlightFilterServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.gridnine.testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FlightFilterServiceImplTest {
+
+    @Test
+    void flightDepartingInThePastRemover() {
+    }
+
+    @Test
+    void flightThatDepartsBeforeItArrivesRemover() {
+    }
+
+    @Test
+    void flightWithMoreThanTwoHoursGroundTimeRemover() {
+    }
+}
